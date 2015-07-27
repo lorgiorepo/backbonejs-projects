fee3a415d07c78b446c3727d6a3e9fdccd26e3fb
Lorgio Trinidad <lorgiotrinidad@gmail.com>
2015-07-26 23:37:38 -0300

    Inicio de version 1.0.0

---
+++ listado/app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global Listado, $*/
+
+
+window.Listado = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    Listado.init();
+});
